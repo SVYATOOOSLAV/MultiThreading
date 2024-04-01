447299bb7dba10f7fe79d06d3e8d58f2e9e5b3dc
Svyatoslav Milkin <svyatoslavmilkin@yandex.ru>
2024-04-01 14:43:10 +0300

    Lock and ReentrantLock

---
+++ src/main/kotlin/org/example/multithreading/Bankomat.kt
@@ -0,0 +1,43 @@
+package org.example.multithreading
+
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+
+fun main(){
+    // trylock используется как условие, если монитор свободный, то поток ставит лок,
+    // если нет, то пропускает синхронизацию
+
+    val lock: Lock = ReentrantLock()
+    Employee("Svyat", lock)
+    Employee("Kirill", lock)
+    Employee("Den", lock)
+    Thread.sleep(5000)
+    Employee("Helena", lock)
+    Employee("Mary", lock)
+}
+
+class Employee(
+    private val name: String,
+    private val lock: Lock
+) : Thread() {
+
+    init{
+        this.start()
+    }
+
+    override fun run(){
+       if (lock.tryLock()){
+           try{
+               println("$name пользуется банкоматом")
+               Thread.sleep(2000)
+               println("$name отошел(а) от банкомата")
+           }
+           finally{
+               lock.unlock()
+           }
+       }
+        else{
+            println("$name не хочет ждать в очереди")
+       }
+    }
+}
---
+++ src/main/kotlin/org/example/multithreading/LockExample.kt
@@ -0,0 +1,70 @@
+package org.example.multithreading
+
+import java.lang.Exception
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+
+fun main(){
+    //ReentrantLock и synchronized блок делают тоже самое
+    // на объекте ReentrantLock всегда необходимо открывать и закрывать монитор объекта
+    val call: Call = Call()
+    val thread1: Thread = Thread{
+        call.mobileCall()
+    }
+    val thread2: Thread = Thread{
+        call.whatsAppCall()
+    }
+    val thread3: Thread = Thread{
+        call.telegramCall()
+    }
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+}
+
+class Call{
+    private val lock: Lock = ReentrantLock()
+
+    fun mobileCall(){
+        lock.lock()
+        try{
+            println("Mobile call starts")
+            Thread.sleep(3000)
+            println("Mobile call ends")
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+        finally {
+            lock.unlock() //unlock всегда необходимо писать в finally для открытия монитора
+        }
+    }
+
+    fun whatsAppCall(){
+        lock.lock()
+        try{
+            println("whatsApp call starts")
+            Thread.sleep(4000)
+            println("whatsApp call ends")
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+        finally {
+            lock.unlock() //unlock всегда необходимо писать в finally для открытия монитора
+        }
+    }
+
+    fun telegramCall(){
+        lock.lock()
+        try{
+            println("telegram call starts")
+            Thread.sleep(5000)
+            println("telegram call ends")
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+        finally {
+            lock.unlock() //unlock всегда необходимо писать в finally для открытия монитора
+        }
+    }
+}
